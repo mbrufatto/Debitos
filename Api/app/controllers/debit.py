dcf33d8b5b5ff4888087dcc885fb54a380267093
Marcio Habigzang Brufatto <mhbrufatto@gmail.com>
2019-02-25 13:48:20 -0300

    Operações básicas do sistema funcionando

--- Api/app/controllers/debit.py
+++ Api/app/controllers/debit.py
@@ -3,6 +3,7 @@ from app import app, db
 from flask import jsonify, json, request, render_template, redirect, url_for
 from app.models.tables import Debit
 from app.models.form import DebitForm
+from datetime import datetime
 
 @app.route('/v1/debit/new', methods=['POST'])
 @app.route('/debit/new', methods=['POST'])
@@ -29,23 +30,46 @@ def search_debit_by_id(debit_id):
     return jsonify([i.serialize for i in debits])
 
 @app.route('/v1/debit/<int:debit_id>/edit', methods=['PUT'])
+@app.route('/debit/<int:debit_id>/edit', methods=['GET', 'POST'])
 def edit_debit(debit_id):
     debit = Debit.query.filter_by(id=debit_id).first()
-    data = request.get_json()
-    debit.client_id = int(data['client_id'])
-    debit.client_name = data['client_name']
-    debit.description = data['description']
-    debit.value = float(data['value'])
-    db.session.commit()
-    debits = Debit.query.filter_by(id=debit_id).all()
-    return jsonify([i.serialize for i in debits]), 201
+    print(str(request.url_rule))
+    if str(request.url_rule) == "/debit/<int:debit_id>/edit":
+        if request.method == 'GET':
+            form = DebitForm(formdata=request.form, obj=debit)
+            debits = Debit.query.all()
+            return render_template('debit.html', form=form, action='edit', debits=debits, debit=debit)
+        else:
+            print("Atualizando")
+            client = request.form['clients'].split('-')
+            debit.client_id = int(client[0])
+            debit.client_name = client[1]
+            debit.description = request.form['description']
+            debit.value = float(request.form['value'])
+            debit.date = datetime.strptime(request.form['date'], '%Y-%m-%d')            
+            db.session.add(debit)
+            db.session.commit()
+            return redirect(url_for('index'))
+    else:
+        data = request.get_json()
+        debit.client_id = int(data['client_id'])
+        debit.client_name = data['client_name']
+        debit.description = data['description']
+        debit.value = float(data['value'])
+        db.session.commit()
+        debits = Debit.query.filter_by(id=debit_id).all()
+        return jsonify([i.serialize for i in debits]), 201
 
 @app.route('/v1/debit/<int:debit_id>', methods=['DELETE'])
+@app.route('/debit/<int:debit_id>', methods=['POST'])
 def delete_debit(debit_id):
     debit = Debit.query.filter_by(id=debit_id).first()
     db.session.delete(debit)
     db.session.commit()
-    return list_debit()
+    if str(request.url_rule) == "/debit/<int:debit_id>":    
+        return redirect(url_for('index'))
+    else:
+        return list_debit()
 
 @app.route('/v1/debits', methods=['GET'])
 def list_debit():
@@ -56,7 +80,8 @@ def list_debit():
 @app.route('/', methods=["GET"])
 def index():
     form = DebitForm()
-    return render_template('debit.html', form=form)
+    debits = Debit.query.all()
+    return render_template('debit.html', form=form, action='add', debits=debits)
 
 
 # @app.route('/hello')
